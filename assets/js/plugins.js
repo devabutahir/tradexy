493cbc5f37fe59efee5b2363333af1ec13772e75
devabutahir <devtahir242@gmail.com>
2025-01-07 15:13:53 +0600

    footer-two

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -113,13 +113,19 @@
                     {
                         breakpoint: 1200,
                         settings: {
+                            slidesToShow: 3,
+                        },
+                    },
+                    {
+                        breakpoint: 992,
+                        settings: {
                             slidesToShow: 2,
                         },
                     },
                     {
                         breakpoint: 768,
                         settings: {
-                            slidesToShow: 1,
+                            slidesToShow: 2,
                         },
                     },
                     {
